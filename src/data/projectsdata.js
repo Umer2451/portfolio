88d5264ba9e0161acc473ff0543e39590658e2a3
Umer2451 <umernaeem44@gmail.com>
2024-07-15 04:11:12 +0500

    finan-app added

--- src/data/projectsdata.js
+++ src/data/projectsdata.js
@@ -1,22 +1,22 @@
 import store from "../images/store.png";
-import html from "../images/html.png"
+import finanApp from "../images/finanApp.png"
 import css from "../images/css.png"
 const projectData = [{
     "id": 1,
     "image":store,
-    "title": "My Fake Store",
+    "title": "My Fake Store (Made just with redux",
     "p": "/fakestore"
 },
 {
     "id": 2,
-    "image": html,
-    "title": "HTML",
-    "p": "/products"
+    "image": finanApp,
+    "title": "Finan App (Made with Redux, React and Firestore)",
+    "p": "/finance-app"
 },
 {
     "id": 3,
-    "image": css,
-    "title": "CSS",
-    "p": "/portfolio"
+    "image": finanApp,
+    "title": "Finan App React Native app",
+    "p": "/finance-app-native"
 }]
 export default projectData;
