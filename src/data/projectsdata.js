b9fbc3b5ba81a185ce739d7d0c8b94b8239062f3
Umer2451 <umernaeem44@gmail.com>
2024-07-15 04:13:07 +0500

    finan-app added

--- src/data/projectsdata.js
+++ src/data/projectsdata.js
@@ -11,12 +11,12 @@ const projectData = [{
     "id": 2,
     "image": finanApp,
     "title": "Finan App (Made with Redux, React and Firestore)",
-    "p": "/finance-app"
+    "p": "/finance-app/"
 },
 {
     "id": 3,
     "image": finanApp,
     "title": "Finan App React Native app",
-    "p": "/finance-app-native"
+    "p": "/finance-app-native/"
 }]
 export default projectData;
